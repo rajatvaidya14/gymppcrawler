69c72a2967a778a0f22aa0204592bc12560d45c3
stalwartrishabh <0601rishabh@gmail.com>
2016-02-04 03:58:59 +0530

    crawler

---
+++ gympp.py
@@ -0,0 +1,39 @@
+import requests
+import lxml.html
+import csv
+import re
+from lxml import etree
+from lxml import html
+
+
+with open('test.csv', 'wb') as csvfile:
+    spamwriter = csv.writer(csvfile, delimiter=',',
+            quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    url = []
+    for x in range(1,135):
+
+            a = "http://gympp.com/ncr/"+str(x)
+            url.append(a)
+            print a
+
+            response = requests.get(a)
+            html = response.content
+            item = etree.HTML(html)
+            name = item.xpath('//*[@class="product_name"]/div/a/@href')
+            print name        
+            for x in name:
+                    b = "http://gympp.com"+x
+                    url.append(b)
+                    print b
+                    response = requests.get(b)
+                    html = response.content
+                    item = etree.HTML(html)
+                    name = item.xpath('//*[@class="prod-name"]/text()')
+                    location = item.xpath('//*[@class="text"]/ul/li/text()')
+                    contact = item.xpath('//*[@class="text"]/ul/li/span/text()')
+                    
+                    for x in range(0,len(name)):
+                    
+                            row = [(name[x].strip()).encode('ascii','ignore')]+[(location[x].strip()).encode('ascii','ignore')]+[(contact[x].strip()).encode('ascii','ignore')]
+                            print row
+                            spamwriter.writerow(row)
